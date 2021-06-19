9f45c3efa1ab5fdc04d891a07941132c00670267
Manna <manna23.sandhu@gmail.com>
2021-06-19 14:56:00 +0530

    added movement

--- Base.js
+++ Base.js
@@ -1,14 +1,15 @@
 AFRAME.registerComponent("base", {
-    schema: {
-      radius: { type: "number", default: 140 },
-      height: { type: "number", default: 4 }
-    },
-    init: function() {
-      this.el.setAttribute("geometry", {
-        primitive: "cylinder",
-        radius: this.data.radius,
-        height: this.data.height
-      });
-      this.el.setAttribute("material", { color: "#1769aa" });
-    }
+  schema: {
+    radius: { type: "number", default: 150 },
+    height: { type: "number", default: 3 }
+  },
+
+  init: function() {
+    this.el.setAttribute("geometry", {
+      primitive: "cylinder",
+      radius: this.data.radius,
+      height: this.data.height
+    });
+    this.el.setAttribute("material", { color: "#1769aa" });
+  }
 });
